3f5c7e8ce6ee0b008cc2afcd336d097342dcab0a
ppvan <phuclaplace@gmail.com>
2023-11-23 11:48:05 +0700

    feat: add profile view mode

--- app/src/main/java/me/ppvan/metour/ui/page/ProfilePage.kt
+++ app/src/main/java/me/ppvan/metour/ui/page/ProfilePage.kt
@@ -10,14 +10,18 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.KeyboardOptions
@@ -25,16 +29,32 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.CameraAlt
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material.icons.outlined.EditNote
 import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -42,6 +62,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import me.ppvan.metour.R
 import me.ppvan.metour.ui.component.rememberImeState
@@ -49,6 +70,7 @@ import me.ppvan.metour.ui.component.rememberImeState
 
 @Composable
 fun ProfilePage() {
+//    ProfileViewPage()
     ProfileEditPage()
 }
 
@@ -62,6 +84,14 @@ fun ProfileEditPage() {
 
         }
     )
+
+    var expand by remember {
+        mutableStateOf(false)
+    }
+    val cities = arrayOf("Hanoi", "Hai Phong", "Quang Ninh")
+    var selected by remember {
+        mutableStateOf(cities[0])
+    }
 
     val formGroup = Modifier
         .fillMaxWidth()
@@ -180,15 +210,205 @@ fun ProfileEditPage() {
                 label = { Text(text = "Số điện thoại") },
                 keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Decimal)
             )
+
+            ExposedDropdownMenuBox(
+                modifier = formGroup,
+                expanded = expand,
+                onExpandedChange = { expand = !expand }) {
+                OutlinedTextField(
+                    value = selected,
+                    label = { Text(text = "Tỉnh/Thành Phố") },
+                    onValueChange = {},
+                    readOnly = true,
+                    trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expand) },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .menuAnchor()
+                )
+
+                ExposedDropdownMenu(
+                    expanded = expand,
+                    onDismissRequest = { expand = false }
+                ) {
+                    cities.forEach { item ->
+                        DropdownMenuItem(
+                            text = { Text(text = item) },
+                            onClick = {
+                                selected = item
+                                expand = false
+//                                Toast.makeText(context, item, Toast.LENGTH_SHORT).show()
+                            }
+                        )
+                    }
+                }
+            }
+
             Button(
                 onClick = { /*TODO*/ },
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(8.dp),
+                    .padding(horizontal = 12.dp)
             ) {
-                Spacer(modifier = Modifier.height(4.dp))
-                Text(text = "Cập nhật Profile")
+                Text(
+                    text = "Cập nhật Profile",
+                    style = MaterialTheme.typography.bodyLarge
+                )
             }
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ProfileViewPage() {
+    Column(Modifier.fillMaxSize()) {
+        TopAppBar(
+            title = {
+
+            },
+            navigationIcon = {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Default.ArrowBack,
+                    contentDescription = "Back"
+                )
+            },
+            actions = {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(imageVector = Icons.Outlined.EditNote, contentDescription = null)
+                }
+            }
+
+        )
+
+        Surface {
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth(0.4f)
+                        .aspectRatio(1f)
+                ) {
+                    Image(
+                        modifier = Modifier
+                            .matchParentSize()
+                            .clip(CircleShape),
+                        painter = painterResource(id = R.drawable.bocchi),
+                        contentDescription = null
+                    )
+                }
+                Spacer(modifier = Modifier.height(6.dp))
+                Text(
+                    text = "@ppvan",
+                    style = MaterialTheme.typography.labelLarge.copy(fontWeight = FontWeight.Bold)
+                )
+
+
+                Spacer(modifier = Modifier.height(48.dp))
+
+                Box(modifier = Modifier.weight(1f), contentAlignment = Alignment.Center) {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxHeight()
+                            .padding(horizontal = 12.dp),
+                        verticalArrangement = Arrangement.spacedBy(8.dp)
+                    ) {
+                        ProfileListItem(
+                            icon = {
+                                Icon(
+                                    imageVector = Icons.Default.Person,
+                                    contentDescription = null,
+                                    tint = MaterialTheme.colorScheme.primary
+                                )
+                            },
+                            label = "Họ và tên",
+                            content = "Phạm Văn Phúc"
+                        )
+
+                        ProfileListItem(
+                            icon = {
+                                Icon(
+                                    imageVector = Icons.Default.Email,
+                                    contentDescription = null,
+                                    tint = MaterialTheme.colorScheme.primary
+                                )
+                            },
+                            label = "Email",
+                            content = "phuclaplace@gmail.com"
+                        )
+
+                        ProfileListItem(
+                            icon = {
+                                Icon(
+                                    imageVector = Icons.Default.Phone,
+                                    contentDescription = null,
+                                    tint = MaterialTheme.colorScheme.primary
+                                )
+                            },
+                            label = "Số điện thoại",
+                            content = "0981234567"
+                        )
+
+                        ProfileListItem(
+                            icon = {
+                                Icon(
+                                    imageVector = Icons.Default.LocationOn,
+                                    contentDescription = null,
+                                    tint = MaterialTheme.colorScheme.primary
+                                )
+                            },
+                            label = "Tỉnh/Thành Phố",
+                            content = "Hà Nội"
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ProfileListItem(
+    icon: @Composable () -> Unit,
+    label: String,
+    content: String
+) {
+    ElevatedCard {
+        Row(
+            modifier = Modifier
+                .height(IntrinsicSize.Max)
+                .padding(horizontal = 4.dp, vertical = 8.dp)
+
+
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(50.dp)
+                    .background(MaterialTheme.colorScheme.surface, shape = CircleShape),
+                contentAlignment = Alignment.Center
+            ) {
+                icon()
+            }
+            Spacer(modifier = Modifier.width(12.dp))
+
+            Column(
+                Modifier
+                    .weight(1f)
+//                .height(50.dp)
+                    .padding(4.dp),
+                verticalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    text = label,
+                    style = MaterialTheme.typography.labelMedium.copy(fontWeight = FontWeight.Light),
+                    color = MaterialTheme.colorScheme.outline
+                )
+                Text(text = content, style = MaterialTheme.typography.labelLarge)
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ProfilePagePreview() {
+    ProfileViewPage()
 }
